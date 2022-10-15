f8e21df1ffdabc5b1f8f032db23b5c3a4201887b
redigebretsi <redigebretsi@gmail.com>
2022-10-15 15:45:05 +0200

    adding variables

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import chart from "chart.js/auto";
+
+const weights = ref([]);
+
+const weightChart1 = ref(null);
+
+const weightChart = shallowRef(null);
+
+const weightInput = ref(60.0);
+
+const currentWeight = computed(() => {
+  return weight.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 };
+});
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput.value,
+    date: new Date.getTime(),
+  });
+};
+</script>
 
 <template>
   <h1>Hello world</h1>
